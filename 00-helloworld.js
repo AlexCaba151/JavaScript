897bb34eedde689734164f5fa6089d0df953c9a5
Yerlin A. Rodriguez <yerlin.alec21@gmail.com>
2024-08-23 12:00:26 -0400

    JavaScript

---
+++ 00-helloworld.js
@@ -0,0 +1,13 @@
+console.log("Hello World!")
+console.log('Hello World')
+
+/*Printing utilizando diferentes tipos comillas*/
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
---
+++ 01-variables.js
@@ -0,0 +1,21 @@
+/*Diferente tipos de Variables en JS*/
+
+//var: Fue el primer tipo que se utilizo pero ya no se recomienda hacerlo
+var helloWorld = "Hello, Javascript!"
+console.log(helloWorld)
+
+helloWorld =("Hola de nuevo, Javascript")
+console.log(helloWorld)
+
+//let: Es el mas comun y el que se recomienda usar actualmente para declarar una variable.
+
+let helloWorld2 = "Hello, Javascript 2"
+console.log(helloWorld2)
+
+helloWorld = ("Hola de nuevo, Javascript 2")
+console.log(helloWorld2)
+
+//const: Se utiliza para declara variable que no cambiaran su valor, osea que son constantes
+
+const helloWorld3 = "Hello, Javascript 3"
+console.log(helloWorld3)
