8a7882f44c905f628cf6b84426b0a38e1bd0a336
Yerlin A. Rodriguez <yerlin.alec21@gmail.com>
2024-08-23 23:59:55 -0400

    exercises

--- 03-exercises.js
+++ 03-exercises.js
@@ -1,10 +1,23 @@
 // 1. Escribe un comentario en una línea
+//Hola esto es un comentario de una linea
 
 // 2. Escribe un comentario en varias líneas
+/*Hola esto es un comentario de varias lineas
+como puedes observar*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let fullName = "Yerlin A. Rodriguez Caba"
+let age = 27
+let dominican = true
+let happiness 
+let goals = null
 
 // 4. Imprime por consola el valor de todas las variables
+console.log(fullName)
+console.log(age)
+console.log(dominican)
+console.log(happiness)
+console.log(goals)
 
 // 5. Imprime por consola el tipo de todas las variables
 
